060b9bc096051c923e16b28c8121592515b6b676
pranavmangalgi <pranavmangalgi@gmail.com>
2024-07-25 10:45:48 +0530

    fix: fixed conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "./components/apollo/apolloClient.js";
 import { BrowserRouter } from "react-router-dom";
